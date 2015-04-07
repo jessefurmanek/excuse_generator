fafd5af339c746ab647352c81a627f8688201c2b
jessefurmanek <jesse.furmanek@gmail.com>
2015-04-07 13:38:30 -0500

    add rating to migration

--- db/migrate/20150329205628_create_join_table_excuse_user.rb
+++ db/migrate/20150329205628_create_join_table_excuse_user.rb
@@ -1,6 +1,7 @@
 class CreateJoinTableExcuseUser < ActiveRecord::Migration
   def change
     create_join_table :excuses, :users do |t|
+    	t.string :rating
       # t.index [:excuse_id, :user_id]
       # t.index [:user_id, :excuse_id]
     end
